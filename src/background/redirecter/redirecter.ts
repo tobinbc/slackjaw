005383e78331f28d4c3a3d39ca0ac00a2b75241b
tobin <tobin.chew89@gmail.com>
2019-07-27 15:10:41 +0100

    updates

--- src/background/redirecter/redirecter.ts
+++ src/background/redirecter/redirecter.ts
@@ -2,32 +2,14 @@
 import URLParse from 'url-parse'
 chrome.webRequest.onHeadersReceived.addListener (
   details => {
-    let responseHeaders = details.responseHeaders;
-    let slackSJON;
-    if (
-      responseHeaders.filter (header => header.name === 'x-slack-response')
-        .length
-    ) {
-      slackSJON = responseHeaders.filter (
-        header => header.name === 'x-slack-response'
-      )[0].value;
-    } else {
-      slackSJON = JSON.stringify ({ok: false});
-    }
+    let slackResponse = details.responseHeaders.find (header => header.name === 'x-slack-response')
+    let  slackSJON = slackResponse ? slackResponse.value : JSON.stringify ({ok: false})
     let url = URLParse(details.url)
-    let state = ''
-    try {
-      state = url.query.state
-    } catch(e){
-      console.log(e) 
-    }
     chrome.tabs.update (details.tabId, {
       url: chrome.extension.getURL (
-        `redirect.html?state=${encodeURIComponent (state)}&slack=${encodeURIComponent (slackSJON)}`
+        `redirect.html?state=${encodeURIComponent (url.query.state || '')}&slack=${encodeURIComponent (slackSJON)}`
       ),
-
     });
-    return;
   },
   {
     urls: [
--- src/popup/components/Holding.tsx
+++ src/popup/components/Holding.tsx
@@ -1,31 +1,41 @@
-import Button from '@material-ui/core/Button';
-import CardActions from '@material-ui/core/CardActions';
+import { CircularProgress } from '@material-ui/core';
 import CardContent from '@material-ui/core/CardContent';
-import Typography from '@material-ui/core/Typography';
-import React, { Fragment, FunctionComponent, useEffect } from 'react';
-import { CircularProgress, CardHeader } from '@material-ui/core';
-import { RouteComponentProps } from 'react-router-dom'
+import React, { FunctionComponent, useEffect, useState } from 'react';
+import { Redirect, RouteComponentProps } from 'react-router-dom';
 
 const getStorage = () => new Promise(resolve => chrome.storage.local.get(resolve))
 
 type Props = RouteComponentProps
 const Holding: FunctionComponent<Props> = (props: Props) => {
-  const [redirect,setRedirect] = useState('')
+  const [redirect, setRedirect] = useState('')
   useEffect(() => {
-    getNextRoute()
+    try {
+      getNextRoute()
+    } catch (e) {
+      setRedirect(`/error/${e.message}`)
+    }
   }, [])
   const getNextRoute = async () => {
-    let storage = await getStorage()
+    let storage:any = await getStorage()
+    if (typeof storage !== 'object') {
+      throw new Error('Storage not found')
+    }
     switch (true) {
-      case Object.keys(storage).length === 0:
-        props.history.push('/signup')
+      case storage.ok === undefined:
+        setRedirect('/signup')
+      case storage.ok === true:
+        setRedirect('/record')
     }
   }
-  return (
-    <CardContent>
-      <CircularProgress style={{ margin: '0 auto' }} />
-    </CardContent>
-  );
+  if (redirect === '') {
+    return (
+      <CardContent>
+        <CircularProgress style={{ margin: '0 auto' }} />
+      </CardContent>
+    );
+  }
+  return <Redirect to={redirect} />
 };
 
 export { Holding };
+
